88925a98f03ffa4552b3ec934128211efe748e18
pychen0918 <pychen0918@gmail.com>
2021-03-13 15:51:31 +0800

    Add a simple bootstrap dashboard

--- src/main/kotlin/com/pychen0918/admin/Application.kt
+++ src/main/kotlin/com/pychen0918/admin/Application.kt
@@ -1,9 +1,38 @@
 package com.pychen0918.admin
 
+
+import freemarker.cache.ClassTemplateLoader
+import freemarker.core.HTMLOutputFormat
 import io.ktor.application.*
 import io.ktor.features.*
+import io.ktor.freemarker.*
+import io.ktor.http.content.*
+import io.ktor.response.*
+import io.ktor.routing.*
+import io.ktor.routing.get
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
 fun Application.module() {
+    install(FreeMarker){
+        templateLoader = ClassTemplateLoader(this::class.java.classLoader, "templates")
+        outputFormat = HTMLOutputFormat.INSTANCE
+    }
+    routing {
+        static("css"){
+            resources("static/css")
+        }
+        static("js"){
+            resources("static/js")
+        }
+        static("assets"){
+            resources("static/assets")
+        }
+        get("/"){
+            call.respond(FreeMarkerContent("index.html", null, ""))
+        }
+        get("/{page}"){
+            call.respond(FreeMarkerContent("${call.parameters["page"]}", null, ""))
+        }
+    }
 }
