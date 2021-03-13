e739e893ca12003558e4c6552e1ba275204cd058
pychen0918 <pychen0918@gmail.com>
2021-03-13 14:00:56 +0800

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,37 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.4.31"
+    application
+}
+
+group = "com.pychen0918"
+version = "0.0.1"
+
+repositories {
+    jcenter()
+    mavenCentral()
+    maven { url = uri("https://dl.bintray.com/kotlin/kotlinx") }
+    maven { url = uri("https://dl.bintray.com/kotlin/ktor") }
+}
+
+dependencies {
+    testImplementation(kotlin("test-junit"))
+    implementation("io.ktor:ktor-server-core:1.5.2")
+    implementation("io.ktor:ktor-server-netty:1.5.2")
+    implementation("io.ktor:ktor-html-builder:1.5.2")
+    implementation("io.ktor:ktor-freemarker:1.5.2")
+    implementation("ch.qos.logback:logback-classic:1.2.3")
+}
+
+tasks.test {
+    useJUnit()
+}
+
+tasks.withType<KotlinCompile>() {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+application {
+    mainClassName = "com.pychen0918.admin.ApplicationKt"
+}
---
+++ src/main/kotlin/com/pychen0918/admin/Application.kt
@@ -0,0 +1,9 @@
+package com.pychen0918.admin
+
+import io.ktor.application.*
+import io.ktor.features.*
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+fun Application.module() {
+}
